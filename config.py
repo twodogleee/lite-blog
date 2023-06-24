d166f99497c1cd71e77cf5f98b899e68e355fab4
twodoglee <twodoglee@msn.com>
2023-06-24 22:04:10 +0800

    [IMP]配置文件

--- config.py
+++ config.py
@@ -4,7 +4,7 @@
 import logging
 
 # sqlLite数据库地址
-DB_PATH: str = 'sqlite:////Users/twodoglee/workspace/py/lite-blog/data/lite_blog.db'
+DB_PATH: str = 'sqlite:///E:\\workspace\\py\\lite-blog\\data\\lite_blog.db'
 # 文件保存地址
-FILE_PATH: str = "/Users/twodoglee/workspace/py/lite-blog/file"
+FILE_PATH: str = "E:\\workspace\\py\\lite-blog\\data\\file"
 LOG_LEVEL = logging.INFO
